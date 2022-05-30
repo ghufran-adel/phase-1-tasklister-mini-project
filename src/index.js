3a1787a64d93e11eccd084a0f079d814ba4b33f5
ghufran adel <gho0ofran.adel14@gmail.com>
2022-05-30 17:16:44 +0300

    assignment has been compeleted

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  const inptVal= document.getElementById("new-task-description");
+  const tasks= document.getElementById("tasks");
+
+
+  form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let li =document.createElement("li");
+    let btn =document.createElement("button");
+    btn.textContent=" X "
+    li.textContent=`${inptVal.value}`
+    li.appendChild(btn);
+    tasks.appendChild(li);
+ btn.addEventListener("click",delet)
+
+  });
+function  delet(e){
+ e.target.parentNode.remove()
+   };
+
+
+
 });
